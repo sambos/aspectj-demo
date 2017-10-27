a7081b4c6dc5fbd2a797f9f6014c5637c7bbc324
Majeed <sam.bos@gmail.com>
2017-10-26 23:32:44 -0500

    Update User.java

---
+++ src/main/java/rsol/aspectj/demo/User.java
@@ -0,0 +1,12 @@
+package rsol.aspectj.demo;
+
+public interface User {
+
+	public void addUser(String name);
+
+	public void deleteUser(String name);
+
+	public String getUser();
+
+	public void listUsers();
+}
